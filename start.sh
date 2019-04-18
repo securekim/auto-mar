8fbeedbf225d360c57639a3c55efdc158a889b3a
securekim <admin@securekim.com>
2019-04-19 01:49:45 +0900

    Add DEBUG

--- start.sh
+++ start.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 #path : /home/maroonsky/devel/bitbucket/maroonsky
-killall node
-sudo killall redis-server
-#/etc/init.d/redis-server stop
 
+###########DEBUG 할 때 주석 해제###############
+#/etc/init.d/redis-server stop
+###########DEBUG 시 주석 처리 할 것#########
+sudo killall redis-server
+sudo killall node
+sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
+cd ../coin-viewer
+NODE_ENV=dev node bin/www&
+sleep 5
+python -mwebbrowser http://127.0.0.1/coin 
+############################################
+sleep 3
 cd ../mar-coin-advisor
 NODE_ENV=simul-min node index.js start&
 sleep 1
