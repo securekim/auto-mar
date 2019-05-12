696e83fdbaf8cbcbdfff0ef30bea486831705895
securekim <brodotkim@gmail.com>
2019-05-12 13:07:17 +0000

    [Feature] Add installer

--- install.sh
+++ install.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
-cd ../coin-viewer
-git pull
-yarn
-cd ../mar-binance-trader
-git pull
-yarn
-cd ../mar-coin-advisor
-git pull
-yarn
+echo "Check projects"
+
+update(){
+	cd $project
+	git pull
+	yarn
+	npm update
+	cd ..
+}
+
+cd ..
+project="mar-binance-trader"
+echo "[$project] CHECK"
+if [ ! -d "$project" ]; then
+	git clone git@bitbucket.org:maroonsky/mar-binance-trader.git
+fi
+update
+
+project="mar-coin-advisor"
+echo "[$project] CHECK"
+if [ ! -d "$project" ]; then
+	git clone git@bitbucket.org:maroonsky/mar-coin-advisor.git
+fi
+update
+
+project="coin-viewer"
+echo "[$project] CHECK"
+if [ ! -d "$project" ]; then
+	git clone git@github.com:securekim/coin-viewer.git
+fi
+update
+
